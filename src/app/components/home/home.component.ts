83c4f78192a6c8cd3e2ea076544ea8b08a5bc51e
senategit <32482980+senategit@users.noreply.github.com>
2018-01-19 12:08:41 +0700

    4
    4

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('now: ', _.now());
+   }
 
   ngOnInit() {
   }
